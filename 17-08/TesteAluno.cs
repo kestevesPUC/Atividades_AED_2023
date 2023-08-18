fbf309cc3d8368dd1733a36d9991ab6565ad9e02
Kaio Gomes Esteves Correa <102831837+kestevesPUC@users.noreply.github.com>
2023-08-17 21:56:14 -0300

    Add files via upload

---
+++ 17-08/TesteAluno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _17_08
+{
+    class TesteAluno
+    {
+        static void Main(string[] args)
+        {
+            Aluno a1 = new Aluno("Kaio", 24, 86.4, 'M');
+            string sexo = a1.getSexo().ToString().ToLower() == "m" ? "Masculino" : "Feminino";
+
+            Console.WriteLine($"Nome do aluno {a1.getNome()}");
+            Console.WriteLine($"Idade do aluno {a1.getIdade()}");
+            Console.WriteLine($"Peso do aluno {a1.getPeso().ToString("#.##")}");
+            Console.WriteLine($"Sexo do aluno {sexo}");
+        }
+    }
+}
