1146226fb6f8e5b62dd741acb31fab659d589db7
Kaio Esteves <1369299@sga.pucminas.br>
2023-08-20 17:54:19 -0300

    update

--- 17-08/TesteAluno.cs
+++ 17-08/TesteAluno.cs
@@ -6,15 +6,35 @@ namespace _17_08
 {
     class TesteAluno
     {
+
+        public static Aluno criaAluno()
+        {
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            int idade = int.Parse(Console.ReadLine());
+            Console.Write("Peso: ");
+            double peso = double.Parse(Console.ReadLine());
+            Console.Write("Sexo: ");
+            char sexo = Console.ReadLine();
+
+            return (new Aluno(nome, idade, peso, sexo)); 
+        }
         static void Main(string[] args)
         {
-            Aluno a1 = new Aluno("Kaio", 24, 86.4, 'M');
+
+            Console.WriteLine("Informe os seguintes dados: ");
+
+            Aluno a1 = criaAluno(); 
             string sexo = a1.getSexo().ToString().ToLower() == "m" ? "Masculino" : "Feminino";
 
             Console.WriteLine($"Nome do aluno {a1.getNome()}");
             Console.WriteLine($"Idade do aluno {a1.getIdade()}");
             Console.WriteLine($"Peso do aluno {a1.getPeso().ToString("#.##")}");
             Console.WriteLine($"Sexo do aluno {sexo}");
+
+            Console.WriteLine();
+            Console.ReadKey();
         }
     }
 }
